530060f0b172955278403907b3721aaa380159fb
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-10-08 00:17:17 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './sass/main.scss';
+import { alert, success, error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import debounce from 'lodash.debounce';
+
+import { refs } from './js/refs';
+// import fetchCountries from './js/fetchCountries';
+
+// export default
+// function fetchCountries(searchQuery) {
+const url = `https://restcountries.com/v2/all?fields=name,capital,population,flag,languages`;
+
+console.log(url);
+
+return fetch(url)
+  .then(res => res.json())
+  .catch(error => error);
+//     .then(response => {
+//   console.log(response);
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return response.json();
+// });
+// }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+// export default function fetchCountries(searchQuery) {
+//   const r = fetch('https://restcountries.com/v2/name').then(response => {
+//     console.log(r);
+//   });
+// }
