b02062fb7550d614626ed27c1882ce553abb07fa
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-10-09 22:44:46 +0300

    try1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './sass/main.scss';
+import countryCardTpl from './templates/country-card.hbs';
+import countryNameTpl from './templates/country-card.hbs';
 import { alert, success, error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import debounce from 'lodash.debounce';
@@ -7,19 +9,41 @@ import { refs } from './js/refs';
 // import fetchCountries from './js/fetchCountries';
 
 // export default
-// function fetchCountries(searchQuery) {
-const url = `https://restcountries.com/v2/all?fields=name,capital,population,flag,languages`;
+function fetchCountries(searchQuery) {
+  const url = `https://restcountries.com/v2/${searchQuery}?fields=name,capital,population,flag,languages`;
 
-console.log(url);
+  // console.log(url);
 
-return fetch(url)
-  .then(res => res.json())
-  .catch(error => error);
-//     .then(response => {
-//   console.log(response);
-//   if (!response.ok) {
-//     throw new Error(response.status);
-//   }
-//   return response.json();
-// });
-// }
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => error);
+  // .then(res => {
+  //     if (res.ok) return res.json();
+  //         throw new Error('Error fetching data')
+  // })
+  // .catch(error => {
+  //     console.error('Error: ', error)
+  // });
+}
+const DEBOUNCE_DELAY = 300;
+refs.input.addEventListener('input', debounce(onPressInput, DEBOUNCE_DELAY));
+// console.log(input);
+function onPressInput(e) {
+  e.preventDefault();
+  const searchQuery = e.target.value.trim();
+  console.log(searchQuery);
+  // if (searchQuery === '') {
+  //   clearData();
+  //   return;
+  // }
+  // fetchCountries(searchQuery)
+  //   .then(data => {
+  //     return data;
+  //   })
+  //   .then();
+}
+
+function clearData() {
+  countryCard.innerHTML = '';
+  countryName.innerHTML = '';
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,1 +1,5 @@
-export const refs = {};
+export const refs = {
+  input: document.querySelector('.input'),
+  countryCardTpl: document.querySelector('.country-list'),
+  countryNameTpl: document.querySelector('.country-container'),
+};
